b155204682996b5c311d2ad982119290600d213b
Wellington Cezario da Silva <WellingtonCezario@outlook.com>
2023-06-01 20:42:22 -0300

    Revert "Introdução a volumes em Docker"
    This reverts commit 2bd1132d854440624ec12afe7546e31853a7cf7e.

---
+++ Curso-Vue/Projetos/projeto-02-calculadora/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1><Calculator/>
+    </div>
+</template>
+
+<script>
+import Calculator from './Main/Calculator'
+
+export default {
+components:{Calculator}
+}
+</script>
+
+<style>
+@font-face {
+    font-family:'Roboto-Mono' ;
+    src: url('./fonts/static/RobotoMono-Thin.ttf');
+}
+*{
+    font-family: 'Roboto-Mono';
+}
+body{
+    margin: 0;
+}
+#app{
+    display: flex;
+    flex-direction:column ;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    
+    color: #FFF;
+    background: linear-gradient(to right, rgb(83,105,118), rgb(41,46,73));
+}
+</style>
