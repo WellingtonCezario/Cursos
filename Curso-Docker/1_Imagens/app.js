2bd1132d854440624ec12afe7546e31853a7cf7e
Wellington Cezario da Silva <WellingtonCezario@outlook.com>
2023-06-01 20:39:49 -0300

    Introdução a volumes em Docker
    Aqui fiz a configuração inicial para aprender volumes em Docker.

--- Curso-Docker/1_Imagens/app.js
+++ Curso-Docker/1_Imagens/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3002
 
 app.get('/', (req,res) =>{
-    res.send('Olá, minha primeira imagem!')
+    res.send('Olá, minha primeira imagem!!')
 })
 
 app.listen(port,()=>{
